033cc0a4430635fa6f5389d54301989b1fe8e768
Yauheni_Mikhnevich <yauheni_mikhnevich@epam.com>
2019-07-12 15:26:07 +0300

    Logging(Logback) configured

--- rettiwt-web/src/main/java/com/training/rettiwt/web/controller/SampleController.java
+++ rettiwt-web/src/main/java/com/training/rettiwt/web/controller/SampleController.java
@@ -1,5 +1,7 @@
 package com.training.rettiwt.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,11 @@ import java.util.Map;
 @RequestMapping("/")
 public class SampleController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("hello")
     public Map<String, String> greetings() {
+        LOGGER.info("hello controller {}", this.getClass().getSimpleName());
         Map<String, String> response = new HashMap<>();
         response.put("greetings", "hello");
         return response;
