733882af1194c4274dd7b85ce81ba17a97013396
Yauheni_Mikhnevich <yauheni_mikhnevich@epam.com>
2019-07-12 18:16:18 +0300

    Logging(Logback) configured

--- rettiwt-web/src/main/java/com/training/rettiwt/web/controller/IndexController.java
+++ rettiwt-web/src/main/java/com/training/rettiwt/web/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.training.rettiwt.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping
 public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @GetMapping
     public String index() {
+        logger.warn("test warn log {}", this.getClass().getSimpleName());
+        logger.debug("test debug log {}", this.getClass().getSimpleName());
+        return "index.html";
+    }
+
+    @GetMapping("error")
+    public String error() {
+        try {
+            throw new IllegalStateException("Custom exeption");
+        } catch (IllegalStateException e) {
+            logger.error("ERROR {}", this.getClass().getSimpleName(), e);
+        }
         return "index.html";
     }
 }
